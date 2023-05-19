d815c0b440efae4765ad0c2fcfa35fb4ed6d07eb
John Doe <dayorito@gmail.com>
2023-05-18 21:35:16 -0500

    version final del formulario aerolinea

--- public/proceso/procesos.js
+++ public/proceso/procesos.js
@@ -2,6 +2,14 @@
 const boton= document.getElementById("boton");
 boton.addEventListener("click", function(e){
     e.preventDefault();
+    try{
+        if (document.getElementById("roundtrip").checked == false && document.getElementById("one-way").checked == false && document.getElementById("multi-city").checked == false){
+            throw new Error("Debe especificar el tipo de viaje a realizar");
+        }else if (document.getElementById("ciudadO").value == "" || document.getElementById("ciudadD").value == ""){
+            throw new Error("Debe especificar la ciudad de origen y destino");
+        }else if (document.getElementById("fechaS").value == false || document.getElementById("fechaR").value == false){
+            throw new Error("Debe especificar fecha de salida y regreso");
+        }
     const fechas= document.getElementById("fechaS").value;
     const fechar= document.getElementById("fechaR").value;
     let fechaActual = new Date();
@@ -15,4 +23,7 @@ boton.addEventListener("click", function(e){
     else{
         alert("Gracias por usar nuestra version de prueba, por favor espere por futuras actualizaciones :)");
     }
+}catch(error){
+    alert("Error: " + error)
+}
 })
